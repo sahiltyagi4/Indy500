706d77b15d30a351e5303ec87806bf585718a578
sahiltyagi4 <sahilt.tyagi@gmail.com>
2019-04-14 04:35:39 -0400

    changed message rate emission to 10 msg/sec

--- src/main/java/com/dsc/iu/utils/ThreemetricSynchronization.java
+++ src/main/java/com/dsc/iu/utils/ThreemetricSynchronization.java
@@ -390,16 +390,28 @@ public class ThreemetricSynchronization {
         outpw = new PrintWriter(outfile);
         
         List<String> carlist = new LinkedList<String>();
-		carlist.add("20");carlist.add("21");carlist.add("13");carlist.add("98");carlist.add("19");carlist.add("33");carlist.add("24");carlist.add("26");carlist.add("7");carlist.add("6");
-	    carlist.add("60");carlist.add("27");carlist.add("22");carlist.add("18");carlist.add("3");carlist.add("4");carlist.add("28");carlist.add("32");carlist.add("59");carlist.add("25");
-	    carlist.add("64");carlist.add("10");carlist.add("15");carlist.add("17");carlist.add("12");carlist.add("1");carlist.add("9");carlist.add("14");carlist.add("23");carlist.add("30");
-	    carlist.add("29");carlist.add("88");carlist.add("66");
+//		carlist.add("20");carlist.add("21");carlist.add("13");carlist.add("98");carlist.add("19");carlist.add("33");carlist.add("24");carlist.add("26");carlist.add("7");carlist.add("6");
+//	    carlist.add("60");carlist.add("27");carlist.add("22");carlist.add("18");carlist.add("3");carlist.add("4");carlist.add("28");carlist.add("32");carlist.add("59");carlist.add("25");
+//	    carlist.add("64");carlist.add("10");carlist.add("15");carlist.add("17");carlist.add("12");carlist.add("1");carlist.add("9");carlist.add("14");carlist.add("23");carlist.add("30");
+//	    carlist.add("29");carlist.add("88");carlist.add("66");
 	    
+        carlist.add("13");
+        
         //FIRST CALL SEQUENTIAL, THEN WRITE PARALLEL VERSION OF IT
+	    int thread=0;
         for(String carnum : carlist) {
-        		callhtm(carnum);
+        		threadrun(carnum, thread);
+        		thread++;
         		System.out.println("completed HTM SEQUENTIAL EXECUTION FOR CAR:" + carnum);
         }
+    }
+    
+    private static void threadrun(String carnum, int threadnum) {
+    		new Thread("thread-"+threadnum+"-for car-"+carnum) {
+    			public void run() {
+    				callhtm(carnum);
+    			}
+    		}.start();
     }
     
     private static void callhtm(String carnum) {
@@ -583,7 +595,7 @@ public class ThreemetricSynchronization {
         	              	throttle_publisher.onNext(racetime + "," + throttle);
         	              	
         	              	//FILE WRITE --> INPUT DATA FOR POST-PROCESSING
-        	              	inpw.println(carnum + "," + racetime + "," + speed + "," + speed_timestamp + "," + rpm + "," + rpm_timestamp 
+        	              	inpw.println(carnum + "_" + racetime + "," + speed + "," + speed_timestamp + "," + rpm + "," + rpm_timestamp 
         	              			+ "," + throttle + "," + throttle_timestamp);
         	              	
         	              	//JSON AGGREGATION AND SYNCHRONIZATION COMES HERE
