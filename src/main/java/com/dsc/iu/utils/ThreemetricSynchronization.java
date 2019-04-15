fb8b9cefeb7a3598889608375f96950f5e732e7c
sahiltyagi4 <sahilt.tyagi@gmail.com>
2019-04-15 15:16:49 -0400

    latency check code for 33 threads

--- src/main/java/com/dsc/iu/utils/ThreemetricSynchronization.java
+++ src/main/java/com/dsc/iu/utils/ThreemetricSynchronization.java
@@ -290,10 +290,11 @@ public class ThreemetricSynchronization {
     public static void main(String[] args) throws IOException {
     		ThreemetricSynchronization ob = new ThreemetricSynchronization();
         List<String> carlist = new LinkedList<String>();
-		carlist.add("20");carlist.add("21");carlist.add("13");carlist.add("98");carlist.add("19");carlist.add("33");carlist.add("24");carlist.add("26");carlist.add("7");carlist.add("6");
-	    carlist.add("60");carlist.add("27");carlist.add("22");carlist.add("18");carlist.add("3");carlist.add("4");carlist.add("28");carlist.add("32");carlist.add("59");carlist.add("25");
-	    carlist.add("64");carlist.add("10");carlist.add("15");carlist.add("17");carlist.add("12");carlist.add("1");carlist.add("9");carlist.add("14");carlist.add("23");carlist.add("30");
-	    carlist.add("29");carlist.add("88");carlist.add("66");
+        carlist.add("22");
+//		carlist.add("20");carlist.add("21");carlist.add("13");carlist.add("98");carlist.add("19");carlist.add("33");carlist.add("24");carlist.add("26");carlist.add("7");carlist.add("6");
+//	    carlist.add("60");carlist.add("27");carlist.add("22");carlist.add("18");carlist.add("3");carlist.add("4");carlist.add("28");carlist.add("32");carlist.add("59");carlist.add("25");
+//	    carlist.add("64");carlist.add("10");carlist.add("15");carlist.add("17");carlist.add("12");carlist.add("1");carlist.add("9");carlist.add("14");carlist.add("23");carlist.add("30");
+//	    carlist.add("29");carlist.add("88");carlist.add("66");
         
         //FIRST CALL SEQUENTIAL, THEN WRITE PARALLEL VERSION OF IT
 	    int thread=0;
